6e193461ee2e0610162c643f1ef10643999be7af
joseph.hughes <joe@joehughes.org>
2017-10-20 14:18:51 +0100

    changing logging level and name in package.json

--- wdio.training.conf.js
+++ wdio.training.conf.js
@@ -61,7 +61,7 @@ exports.config = {
     //
     // Level of logging verbosity: silent | verbose | command | data | result |
     // error
-    logLevel: 'verbose',
+    logLevel: 'error',
     //
     // Enables colors for log output.
     coloredLogs: true,
